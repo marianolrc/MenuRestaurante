d29eb3d7f4fcd9a00d4a914532a48d363a3c6b0f
marianolrc <marianoagonzalez95@gmail.com>
2023-07-16 21:43:23 -0300

    Inicializacion de Proyecto, Menu Restaurante V0.1

---
+++ menu/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="container">
+    <div className="row">
+        <div className="col">
+            <h1>Home</h1>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default Home
