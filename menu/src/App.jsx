e2f1b2b778d897b715eb74397c6c5308c9bd926a
marianolrc <marianoagonzalez95@gmail.com>
2023-08-18 20:46:17 -0300

    Agregada pagina Admin con su respectivo esqueleto

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -3,15 +3,15 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomeScreen from './pages/Home';
 import NavBarMenu from './components/NavBarMenu';
 import PageNotFound from './pages/Error404';
-import './App.css'
+import Admin from './pages/Admin';
 
 const App = () => {
   return (
-    <div className='App'>
+    <div>
     <BrowserRouter>
-
     <Routes>
       <Route path='/' element={<HomeScreen/>}/>
+      <Route path='/admin' element={<Admin/>}/>
       <Route path='*' element={<PageNotFound/>}/>
     </Routes>
     
