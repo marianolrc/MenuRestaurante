2c5cb4f835276d551f2cdaa6667127d0dc0e17cb
marianolrc <marianoagonzalez95@gmail.com>
2023-08-18 20:50:14 -0300

    Actualizando ramas

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomeScreen from './pages/Home';
-import NavBarMenu from './components/NavBarMenu';
 import PageNotFound from './pages/Error404';
 import Admin from './pages/Admin';
 import './App.css'
