69afb918138ea869406f4e348eae1af45821e88c
axelquiroga96 <axel.quiroga.96@gmail.com>
2023-08-19 20:02:08 -0300

    Se realizó modificaciones en las importaciones.

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomeScreen from './pages/Home';
 import PageNotFound from './pages/Error404';
-<<<<<<< HEAD
-import Admin from './pages/Admin';
-=======
 import MenuComidas from './pages/MenuComidas';
->>>>>>> 7dd55420622615fc2a3c5ed71936294c9c55c19b
 import './App.css'
 import Home from './pages/Home';
 
