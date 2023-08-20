1a5e5d8003077a89c1b2d5ef331eee5d2fdb03b0
Nicolas Maggia <nickmaggia20@gmail.com>
2023-08-20 07:57:51 -0300

    Agregando cards en menu, cardPlatosPrincipales

--- menu/src/components/CardPlatosPrincipales.jsx
+++ menu/src/components/CardPlatosPrincipales.jsx
@@ -5,6 +5,10 @@ import Banner from '../assets/img/milanesas_pure.jpg'
 import Banner2 from '../assets/img/pizza.jpg'
 import sanguchevacio from '../assets/img/sanguchevacio.jpg'
 import bistec from '../assets/img/bistec.jpg'
+import sorrentinos from '../assets/PlatosPrincipales/sorrentinos.jpg'
+import locro from '../assets/PlatosPrincipales/locro.jpg'
+import humita from '../assets/PlatosPrincipales/humita.jpg'
+import lasaña from '../assets/PlatosPrincipales/lasaña.jpg'
 
 function CardPlatosPrincipales() {
   return (
@@ -37,6 +41,63 @@ function CardPlatosPrincipales() {
         </Card.Body>
       </Card>
       </div>
+        <div className='m-2'>
+      <Card style={{ width: '16rem' }} className='bg-dark'>
+        <Card.Img variant="top" src={humita} alt="Humita de Olla" height="180" />
+        <Card.Body>
+          <Card.Title className='text-center text-light'>Humita de Olla</Card.Title>
+          <Card.Text className='text-center'>
+          Humita preparada con el choclo mas dulce y abundante queso. 
+          </Card.Text>
+          <div className='d-flex justify-content-center mt-4'>
+          <Button variant="success">Agregar</Button>
+          </div>
+        </Card.Body>
+      </Card>
+      </div>
+        <div className='m-2'>
+      <Card style={{ width: '16rem' }} className='bg-dark'>
+        <Card.Img variant="top" src={lasaña} alt="Lasaña" height="180" />
+        <Card.Body>
+          <Card.Title className='text-center text-light'>Lasaña</Card.Title>
+          <Card.Text className='text-center'>
+          Lasaña preparada con la mejor carne, queso premium y salsa especial.  
+          </Card.Text>
+          <div className='d-flex justify-content-center mt-4'>
+          <Button variant="success">Agregar</Button>
+          </div>
+        </Card.Body>
+      </Card>
+      </div>
+        <div className='m-2'>
+      <Card style={{ width: '16rem' }} className='bg-dark'>
+        <Card.Img variant="top" src={locro} alt="Lasaña" height="180" />
+        <Card.Body>
+          <Card.Title className='text-center text-light'>Locro</Card.Title>
+          <Card.Text className='text-center'>
+          Locro preparada con la mejor carne. Los mejor para darte ese sabor a casa.  
+          </Card.Text>
+          <div className='d-flex justify-content-center mt-4'>
+          <Button variant="success">Agregar</Button>
+          </div>
+        </Card.Body>
+      </Card>
+      </div>
+        <div className='m-2'>
+      <Card style={{ width: '16rem' }} className='bg-dark'>
+        <Card.Img variant="top" src={sorrentinos} alt="Sorrentinos" height="180" />
+        <Card.Body>
+          <Card.Title className='text-center text-light'>Sorrentinos</Card.Title>
+          <Card.Text className='text-center'>
+          Sorrentinos de carne de cabra. Preparados con una salsa especial y abundante queso.   
+          </Card.Text>
+          <div className='d-flex justify-content-center mt-4'>
+          <Button variant="success">Agregar</Button>
+          </div>
+        </Card.Body>
+      </Card>
+      </div>
+        
     <div className='m-2'>
       <Card style={{ width: '16rem' }} className='bg-dark'>
         <Card.Img variant="top" src={Banner} alt="Papas Cerveceras" height="180" />
