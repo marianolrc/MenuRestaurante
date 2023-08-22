c632a73832107b52bd36c835e31487df6d0bd0a2
marianolrc <marianoagonzalez95@gmail.com>
2023-08-21 21:35:08 -0300

    Formulario Alta productos y Tabla usuarios completa - Falta aplicar css en algunas cosas

--- menu/src/components/FormMenu.jsx
+++ menu/src/components/FormMenu.jsx
@@ -1,5 +1,5 @@
 import {useState, useRef} from 'react'
-
+import '../components/FormMenu.css'
 
 export default function FormMenu(){
     const [datos, setDatos] = useState({
@@ -8,7 +8,7 @@ export default function FormMenu(){
         nombre_producto: '',
         descripcion:'',
         precio: '',
-        imagen: ''
+        imagen: null
     })
 
     const [selectedImage, setSelectedImage] = useState(null);
@@ -42,33 +42,59 @@ export default function FormMenu(){
           precio: e.target.value,
         }));
       }
+
+      function procesar(e) {
+        e.preventDefault();
+        
+        if (!selectedImage) {
+          alert("Seleccione una imagen antes de cargar los datos.");
+          return;
+        }
+      
+        alert(`Dato Cargado ${datos.nombre_producto}, ${datos.descripcion} y ${datos.precio}`);
+      }
+      
       
     return(
-        <div>
-            <form >
-                <p>Seleccione la Categoria del Producto:</p>
-                <select name="catProducto" id="catProducto">
-                    <option value="comidas">Comidas</option>
-                    <option value="bebidas">Bebidas</option>
-                    <option value="postres">Postres</option>
-                </select>
-                <p>Seleccione la Subcategoria del Producto:</p>
-                <select name="subCatProducto" id="subCatProducto">
-                    <option value="minutas">Minutas</option>
-                </select>
-                <p>Nombre del Producto:</p>
-                <input type="text" name="nombreProducto" id="nombreProducto" value={datos.nombre_producto} onChange={cambioProducto} />
-                <p>Descripción para el Producto</p>
-                <input type="text" name="descProducto" id="descProducto" value={datos.descripcion} onChange={cambioDescripcion} />
-                <p>Precio del Producto:</p>
-                <input type="number" name="precioProducto" id="precioProducto" value={datos.precio} onChange={cambioPrecio} />
-                <p>Seleccione una imagen para asociar al producto:</p>
-                <input type="file" accept="image/*" onChange={handleImageSelect} ref={subirImagen} style={{ display: 'none' }}  />
-                <button onClick={() => subirImagen.current.click()}>Seleccionar Imagen</button>
-                {selectedImage && <p>Imagen seleccionada: {selectedImage.name}</p>}
 
-
-            </form>
-        </div>
+        <div className='container-fluid formulario'>
+        <form onSubmit={procesar}>
+          <div className='form-group'>
+            <label htmlFor='catProducto'>Seleccione la Categoria del Producto:</label>
+            <select name='catProducto' id='catProducto'>
+              <option value="comidas">Comidas</option>
+              <option value="bebidas">Bebidas</option>
+              <option value="postres">Postres</option>
+            </select>
+          </div>
+          <div className='form-group'>
+            <label>Seleccione la Subcategoria del Producto:</label>
+            <select name='subCatProducto' id='subCatProducto'>
+            <option value="minutas">Minutas</option>
+            </select>
+          </div>
+          <div className='form-group'>
+            <label>Nombre del Producto:</label>
+            <input type='text' name='nombreProducto' id='nombreProducto' value={datos.nombre_producto} onChange={cambioProducto} />
+          </div>
+          <div className='form-group'>
+            <label>Descripción para el Producto:</label>
+            <input type='text' name='descProducto' id='descProducto' value={datos.descripcion} onChange={cambioDescripcion} />
+          </div>
+          <div className='form-group'>
+            <label>Precio del Producto:</label>
+            <input type='number' name='precioProducto' id='precioProducto' value={datos.precio} onChange={cambioPrecio} />
+          </div>
+          <div className='form-group'>
+            <label>Seleccione una imagen para asociar al producto:</label>
+            <input type='file' accept='image/*' onChange={handleImageSelect} ref={subirImagen} style={{ display: 'none' }} />
+            <button onClick={() => subirImagen.current.click()}>Seleccionar Imagen</button>
+            {selectedImage && <p>Imagen seleccionada: {selectedImage.name}</p>}
+          </div>
+          <div className='form-group'>
+            <input type='submit' value='Cargar Producto' />
+          </div>
+        </form>
+      </div>
     )
 }
