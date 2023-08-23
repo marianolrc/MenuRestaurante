87fa30525eae1cfadf59f1860e4b88dde1e9aee1
Nicolas Maggia <nickmaggia20@gmail.com>
2023-08-23 10:57:19 -0300

    Se crearon los componentes para realizar pagos, pagoefectivo, pagocredito, pagodebito, pagotransferencia. Tambien se creo pagina de pagos

--- menu/src/pages/Menu.jsx
+++ menu/src/pages/Menu.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import NavBarMenu from '../components/NavBarMenu.jsx'
 
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import CardEntradas from '../components/cardsEntradas.jsx';
-import CardPlatosPrincipales from '../components/cardPlatosPrincipales.jsx';
-import CardsPostres from '../components/CardsPostres.jsx';
-import CardsBebidas from '../components/CardsBebidas.jsx';
 import Footer from '../components/Footer.jsx';
 import NavbarLog from '../components/NavbarLog.jsx';
 import '../components/NavBarMenu.css'
+import Pagos from './Pagos.jsx';
+import PagoEfectivo from '../components/modalPagosEfectivo.jsx';
 
 
 const Menu = () => {
@@ -19,24 +15,14 @@ const Menu = () => {
         <NavbarLog/>
 
         <div >
-    {/*     <Tabs
-      defaultActive="profile"
-      id="uncontrolled-tab-example"
-      className="mb-3 d-flex justify-content-center"
-    >
-      <Tab eventKey="Entradas" title="Entradas" >
-        <CardEntradas></CardEntradas>
-      </Tab>
-      <Tab eventKey="Platos Principales" title="Platos Principales">
-       <CardPlatosPrincipales></CardPlatosPrincipales>
-      </Tab>
-      <Tab eventKey="Postres" title="Postres" >
-        <CardsPostres></CardsPostres>
-      </Tab>
-      <Tab eventKey="Bebidas" title="Bebidas" >
-        <CardsBebidas></CardsBebidas>
-      </Tab>
-    </Tabs> */}
+         {<Pagos /> }
+         
+         
+        <div>
+
+        </div>
+      
+ 
 
     <div className='container-fluid bg-dark mt-3'>
     <Footer/>
