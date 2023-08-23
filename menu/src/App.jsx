1653e051a4fa8644302255d6840cc3c5de1435a5
marianolrc <marianoagonzalez95@gmail.com>
2023-08-23 12:47:34 -0300

    Contenido actualizado con pull de rama de Paola

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomeScreen from './pages/Home';
 import PageNotFound from './pages/Error404';
 import './App.css'
-import MenuBotones from './pages/Plantilla_cliente';
+import Admin from './pages/Admin';
+import MenuComidas from './pages/MenuComidas';
 
 const App = () => {
   return (
